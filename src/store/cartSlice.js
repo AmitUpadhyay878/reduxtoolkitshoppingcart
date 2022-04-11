86b2927b5f960edbd92e16edb62d744269ad6969
Amitupadhyay878 <amitupadhyay878@gmail.com>
2022-04-11 23:39:05 +0530

    if item exist in cart then only qty is increase when click on add t cart button from Home

--- src/store/cartSlice.js
+++ src/store/cartSlice.js
@@ -2,39 +2,63 @@ import { createSlice } from "@reduxjs/toolkit";
 
 // const initialState =[];
 
+const cartSlice = createSlice({
+  name: "cart",
+  // initialState:{
+  //    cart:[],
+  //    products:[]
+  // },
+  initialState: [],
+  totalPrice: 0,
+  totalQuantities: 0,
+  reducers: {
+    add(state, { payload }) {
+      const { id } = payload;
+      const ExistID = state.find((item) => item.id === id);
 
-const cartSlice=createSlice(
-    {
-        name:"cart",
-        // initialState:{
-        //    cart:[],
-        //    products:[]
-        // },
-        initialState:[],
-        totalPrice: 0,
-        totalQuantities: 0,
-        reducers:{
-            add(state,action){
+      if (ExistID) {
+        return state.map((item) => {
+          if (item.id === id)
+            return {
+              ...item,
+              qty: item.qty + 1,
+            };
+          return item;
+        });
+      } else {
+        //old Redux
+        //   return[...state,action.payload]
 
-                        //old Redux
-                        //   return[...state,action.payload]
-                     
-                        //new in reduxToolkit
-                        // in reduxtoolkit slice  is allow to write that directlly 
-                        //createSlice method internally works to return[...state,action.payload]
-                        
-                        
-                        
-                        state.push(action.payload)
-            },
-            remove(state,action){
-              return state.filter(item=>item.id !==action.payload)
+        //new in reduxToolkit
+        // in reduxtoolkit slice  is allow to write that directlly
+        //createSlice method internally works to return[...state,action.payload]
+
+        // state.push(action.payload)
+        state.push({ ...payload });
+      }
+    },
+    remove(state, action) {
+      return state.filter((item) => item.id !== action.payload);
+    },
+    inc({ state, payload }) {
+
+        return state.map((item) =>{
+            if(item.id ===payload){
+                return{...item,qty:item.qty+1}
             }
-            
-           
-        }
-    }
-)
+            return item
+        })
+    },
+    dec({ state, payload }) {
+        return state.map((item) =>{
+            if(item.id ===payload){
+                return{...item,qty:item.qty-1}
+            }
+            return item
+        })
+    },
+  },
+});
 
-export const {add,remove} =cartSlice.actions;
+export const { add, remove } = cartSlice.actions;
 export default cartSlice.reducer;
